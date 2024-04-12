1f8e7a119e169a53ae2830d26504caebe866d38f
Ram Pukar <ram_mosone@yahoo.com>
2024-04-12 23:45:02 +0545

    [FIXED]:: Image deleted

--- resources/views/backends/_layout.blade.php
+++ resources/views/backends/_layout.blade.php
@@ -20,7 +20,7 @@
 <header class="nav-header">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">GoodVibes</a>
+            <a class="navbar-brand" href="{{ route('dashboard') }}">Global Security</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
